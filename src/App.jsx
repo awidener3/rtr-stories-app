f524f97c1ac8dc00dcc4676a04db826456608176
Alex Widener <awidener3@gmail.com>
2023-04-26 16:57:00 -0400

    Linting with ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
 // ! Notes at bottom
-import * as React from 'react';
-
-// This will only be defined once since it is outside of the functional component
-const welcome = {
-	title: 'React',
-	greeting: 'Hello',
-	colors: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'],
-};
-
 function App() {
-	// This would be redefined anytime this functional component renders/runs
-	// const title = 'React';
-
-	function getTitle(title) {
-		return `${welcome.greeting} ${title}`;
-	}
-
 	return (
 		<div>
-			<h1>{getTitle('React')}</h1>
-
-			<p>Rainbow Colors:</p>
-			<ul>
-				{welcome.colors.map((color) => (
-					<li>{color}</li>
-				))}
-			</ul>
+			<h1>Hello World</h1>
 
 			<label htmlFor="search">Search: </label>
 			<input type="text" id="search" />
@@ -42,4 +19,6 @@ export default App;
   > UPDATES: Any time a file changes the dev server reloads all affected files for browser. Uses "React Fash Refresh" on react's side and "Hot Module Replacement" on dev server side
 
   > REACT HTML ATTRIBUTES: https://legacy.reactjs.org/docs/dom-elements.html#all-supported-html-attributes
+
+  > ESLINT: install dev dependency `vite-plugin-eslint` and integrate into vite.config.js
  */
