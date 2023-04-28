da79d4522f3fcd5d0d4ee8e1f69c6c2d0a027ec4
Alex Widener <awidener3@gmail.com>
2023-04-28 09:05:50 -0400

    React Component Composition

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,9 @@ const App = () => {
 		<div>
 			<h1>My Hacker Stories</h1>
 
-			<InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch} />
+			<InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch}>
+				<strong>Search:</strong>
+			</InputWithLabel>
 
 			<hr />
 
@@ -52,10 +54,11 @@ const App = () => {
 	);
 };
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => {
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => {
 	return (
 		<>
-			<label htmlFor={id}>{label}</label> &nbsp;
+			<label htmlFor={id}>{children}</label>
+			&nbsp;
 			<input id={id} type={type} value={value} onChange={onInputChange} />
 		</>
 	);
